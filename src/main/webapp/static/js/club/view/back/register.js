e5d7827f8369a65c9324943b443b349b7f604173
wade <wade@hyx.com>
2017-03-23 21:11:51 +0800

    registry tip Signed-off-by: wade <wade@hyx.com>

--- src/main/webapp/static/js/club/view/back/register.js
+++ src/main/webapp/static/js/club/view/back/register.js
@@ -60,7 +60,7 @@ const NormalRegisterForm = Form.create()(React.createClass({
     };
     return (
       <Form onSubmit={this.handleSubmit} className="register-form">
-        (后台功能正在开发中，只有部分功能可用，且还未影响前台数据。数据将可能被清楚)
+        (后台功能正在开发中，只有部分功能可用。数据将可能被清除)
         <FormItem
         	{...formItemLayout}
           label="Account"
